d005363eb0bb755797da2e239ba244f4b99427e8
Baiying Lu <bl223@duke.edu>
2020-02-25 22:46:40 -0500

    Drop out the missing data pair if missing in voltage

--- test_ECG_processor.py
+++ test_ECG_processor.py
@@ -34,3 +34,22 @@ def test_if_missing_time(a, b, expected1, expected2):
     answer1, answer2 = if_missing_time(a, b)
     assert answer1 == expected1
     assert answer2 == expected2
+
+
+@pytest.mark.parametrize("a, b, expected1, expected2", [
+    ([1, 2, nan, 4, 5], [0.1, 0.2, 0.3, 0.4, 0.5],
+     [1, 2, nan, 4, 5], [0.1, 0.2, 0.3, 0.4, 0.5]),
+    ([2.2, 3.3, 4.4, 5.5], [1.1, 1.2, nan, 1.4],
+     [2.2, 3.3, 5.5], [1.1, 1.2, 1.4]),
+    ([nan, 3.3, 4.4, 5.5], [1.1, 1.2, nan, 1.4],
+     [nan, 3.3, 5.5], [1.1, 1.2, 1.4]),
+    ([nan, 3.3, 4.4, nan], [1.1, 1.2, nan, 1.4],
+     [nan, 3.3, nan], [1.1, 1.2, 1.4]),
+    ([nan, 3.3, 4.4, nan], [nan, 1.2, nan, 1.4],
+     [3.3, nan], [1.2, 1.4]),
+])
+def test_if_missing_vol(a, b, expected1, expected2):
+    from ECG_processor import if_missing_vol
+    answer1, answer2 = if_missing_vol(a, b)
+    assert answer1 == expected1
+    assert answer2 == expected2
