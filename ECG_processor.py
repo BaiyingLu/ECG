72aee89e5f6512934076e14fa691914e37839cda
Baiying Lu <bl223@duke.edu>
2020-02-25 20:59:12 -0500

    not necessary

--- ECG_processor.py
+++ ECG_processor.py
@@ -6,11 +6,9 @@ import math
 
 def interface():
     """Take in the data file name
-
     This function is an interface which can interact with the user. This
     function takes in the file path with file extension.
     Then it calls the function import_data and tsh_info_process
-
     """
     print("Please input the document path:")
     print("Please include the extension,like: .txt")
