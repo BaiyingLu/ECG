c6a12883b05b2a791e36af3f32bdb6d8d58ee999
Baiying Lu <bl223@duke.edu>
2020-02-25 21:09:15 -0500

    Convert the string from path to filename and set the logging file

--- ECG_processor.py
+++ ECG_processor.py
@@ -2,19 +2,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import logging
+import ntpath
+
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
 
 
 def interface():
     """Take in the data file name
-
     This function is an interface which can interact with the user. This
     function takes in the file path with file extension.
     Then it calls the function import_data and tsh_info_process
-
     """
     print("Please input the document path:")
     print("Please include the extension,like: .txt")
     path = input("The path is: ")
+    file_name = path_leaf(path)
+    logging.basicConfig(filename=file_name + '.log',
+                        level=logging.INFO,
+                        filemode='w')
     data = take_in_data(path)
 if __name__ == "__main__":
     interface()
